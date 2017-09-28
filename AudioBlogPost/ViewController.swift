9077e1ec82f468fd80c866f074f4d92eebe156a3
pattypatpat2632 <oleary.audio@gmail.com>
2017-09-28 17:17:57 -0400

    working time shift

--- AudioBlogPost/ViewController.swift
+++ AudioBlogPost/ViewController.swift
@@ -7,23 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var audioEngine = AVAudioEngine()
+    var playerNode = AVAudioPlayerNode()
+    var buffer: AVAudioBuffer?
+    var frameCount: AVAudioFrameCount = 30000
+    var player: AVAudioPlayer?
+    let changePitch = AVAudioUnitTimePitch()
+    
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var playButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setup()
+    }
+    
+    func setup() {
+        label.text = "120"
+        audioEngine.attach(playerNode)
+        audioEngine.attach(changePitch)
+        audioEngine.connect(playerNode, to: changePitch, format: nil)//format can be buffer.format
+        audioEngine.connect(changePitch, to: audioEngine.mainMixerNode, format: nil)//format can be buffer.format
+        audioEngine.prepare()
+        try! audioEngine.start()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     @IBAction func sliderValueChanged(_ sender: UISlider) {
-        label.text = String(slider.value)
+        label.text = String(sender.value)
+        changePitch.rate = sender.value/120
+        
+        
     }
+    
+    @IBAction func playButtonTapped(_ sender: Any) {
+        let url = Bundle.main.url(forResource: "keys", withExtension: ".wav")
+        if let url = url {
+            
+            do {
+                let audioFile = try AVAudioFile(forReading: url)
+                changePitch.rate = slider.value/120
+                playerNode.scheduleFile(audioFile, at: nil, completionHandler: nil)
+            } catch {
+                
+            }
+        }
+        playerNode.play()
+    }
+
 
 
 }
