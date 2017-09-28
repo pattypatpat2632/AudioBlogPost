e632520fe24db3690766c19f2f5871be20bc94c2
pattypatpat2632 <oleary.audio@gmail.com>
2017-09-28 16:10:10 -0400

    Initial Commit

---
+++ AudioBlogPost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioBlogPost
+//
+//  Created by Patrick O'Leary on 9/28/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
