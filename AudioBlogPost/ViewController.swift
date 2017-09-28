3e8cbdd1e45d14be6a1ba5ed85eaf5402780aa2d
pattypatpat2632 <oleary.audio@gmail.com>
2017-09-28 16:15:39 -0400

    add slider and label

--- AudioBlogPost/ViewController.swift
+++ AudioBlogPost/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,9 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        label.text = String(slider.value)
     }
 
 
